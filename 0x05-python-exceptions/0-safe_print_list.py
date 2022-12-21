f03ff448023a1cdbb36e4f4aeaa419705bf0aaa6
pprimez <abrahamoba78@gmail.com>
2022-12-20 19:47:12 -0800

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             c += 1
-        except IndexError:
+        except TypeError:
             break
-    print(c)
+    print()
     return c 
